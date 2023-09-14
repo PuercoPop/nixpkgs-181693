11eefd9e9c25ee82871ddeee1368024ae0cb0b38
Javier Olaechea <pirata@gmail.com>
2023-09-14 09:37:25 -0500

    There was no issue. Show how fix the scenario instead

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# To reproduce
+# Nix: How to handle Gemfile other files when using bundlerEnv
+
+If your Gemfile references other files, such as .ruby-verison, .tools-version or
+vendored gems, bundlerEnv will run into issues as it copies the Gemfile and
+Gemfile.lock to a separate directory in the store. When evaluating the Gemfile
+you'll run into errors as the other files are not present. To fix we need to use
+the `extraConfigPaths` option for bundlerEnv. In `shell.nix` you'll find a small
+setup to show you how its done.
 
 ```shell
-nix-shell
-rackup
+nix-shell -A default
+rackup # the pudding
 ```
 
-# To test the fix
 
-```shell
-nix-shell --arg nixpkgs $HOME/src/nixpkgs
-rackup
-```
+Additionally, bundler started supporting a keyword argument to the ruby method
+named file: in version 2.4.19. So if your Gemfile has `ruby file: .ruby-verison`
+you'll need to ensure that the bundler is 2.4.19 or higher. This might involve
+updating the bundler version used by bundlerEnv. I've also included it in the
+example.
--- shell.nix
+++ shell.nix
@@ -1,29 +1,57 @@
-{ system ? builtins.currentSystem,
-  nixpkgs ? fetchTarball {
+{ system ? builtins.currentSystem
+, nixpkgs ? fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz";
     sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
-  },
-  pkgs ? import nixpkgs {
-    overlays = [];
-    config = {};
+  }
+, pkgs ? import nixpkgs {
+    overlays = [ ];
+    config = { };
     inherit system;
   }
 }:
 let
-  ruby = pkgs.ruby_3_2;
-  gems = pkgs.bundlerEnv {
+  ruby = pkgs.ruby_3_2.override {
+    bundler = pkgs.bundler;
+  };
+  bundler = pkgs.buildRubyGem {
+    inherit ruby;
+    name = "$bundler-2.4.19";
+    gemName = "bundler";
+    version = "2.4.19";
+    source.sha256 = "sha256-M03HlkODhHMv3xm/ovYjdTt+2FFg0Izh8gAJmEzvs2I=";
+    dontPatchShebangs = true;
+
+    postFixup = ''
+      sed -i -e "s/activate_bin_path/bin_path/g" $out/bin/bundle
+    '';
+  };
+
+  gems = pkgs.bundlerEnv.override { inherit bundler; } {
     name = "gems";
     inherit ruby;
     gemfile = ./Gemfile;
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
-    gemfileExtraFiles = [ ./.ruby-version ];
     extraConfigPaths = [ "${./.}/.ruby-version" ];
   };
 in
-pkgs.mkShell {
-  buildInputs = [
-    gems
-    gems.wrappedRuby
-  ];
+{
+  default = pkgs.mkShell {
+    buildInputs = [
+      gems
+      gems.wrappedRuby
+    ];
+  };
+  bundler = pkgs.mkShell {
+    buildInputs = [
+      pkgs.bundix
+      bundler
+    ];
+  };
+  update = pkgs.mkShell {
+    buildInputs = [
+      pkgs.bundix
+    ];
+  };
+  pkgs = pkgs;
 }
